c22356ac3538ca774cea7ec737182342cc20665a
David Proto <davidproto@sbcglobal.net>
2023-04-01 19:43:59 -0400

    sliders working for scroll function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,14 @@ import requests from '../Requests'
 
 const Home = () => {
   return (
-    <>
+    <>rowId = '1' 
       <Main />
-      <Row title='Popular' fetchURL={requests.requestPopular} />
-      <Row title='Top Rated' fetchURL={requests.requestTopRated}/>
-      <Row title='Trending' fetchURL={requests.requestTrending}/>
-      <Row title='Comedy' fetchURL={requests.requestComedy}/>
-      <Row title='Horror' fetchURL={requests.requestHorror}/>
-      <Row title='Up Coming' fetchURL={requests.requestUpcoming}/>
+      <Row rowID = '1' title='Popular' fetchURL={requests.requestPopular} />
+      <Row rowID = '2' title='Top Rated' fetchURL={requests.requestTopRated}/>
+      <Row rowID = '3' title='Trending' fetchURL={requests.requestTrending}/>
+      <Row rowID = '4' title='Comedy' fetchURL={requests.requestComedy}/>
+      <Row rowID = '5' title='Horror' fetchURL={requests.requestHorror}/>
+      <Row rowID = '6' title='Up Coming' fetchURL={requests.requestUpcoming}/>
 
     </>
   )
